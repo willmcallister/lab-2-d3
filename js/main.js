a1c2efce1e4471d8c29bf0bfbd3b988db86f0c49
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-03 15:43:43 -0500

    loaded data and added topojson.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-// coming soon -- week 9
 
-/*
 //execute script when window is loaded
-window.onload = function(){
+window.onload = setMap();
 
-}
-*/
+// set up choropleth map
+function setMap(){
+
+    // use Promise.all to load all data asyncronously
+    var promises = [d3.csv("data/csv/commute_mode_share_by_state.csv"),
+                    d3.json("data/us_states_ne_generalized.topojson"),
+                    d3.json("data/countries_ne_50m.topojson")
+                    ];
+    Promise.all(promises).then(callback);
+
+    function callback(data){
+        commuteMode = data[0];
+        countries = data[1];
+        states = data[2];
+        console.log(commuteMode);
+        console.log(countries);
+        console.log(states);
+    }
+};
