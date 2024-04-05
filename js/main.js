f78859dbfb5c6eef623de9c997f51c59357bfa96
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-04 21:08:52 -0500

    Activity 9
    Completed Activity 9 - created a D3 basemap of the US with a states layer which will be turned into a choropleth map in future updates

--- js/main.js
+++ js/main.js
@@ -5,32 +5,42 @@ window.onload = setMap();
 // set up choropleth map
 function setMap(){
 
-    //map frame dimensions
-    var width = 960,
-        height = 460;
+    // map frame dimensions
+    var width = 900,
+        height = 600;
 
-    //create new svg container for the map
+    // create new svg container for the map
     var map = d3.select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection centered on France
-    var projection = d3.geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
-        .translate([width / 2, height / 2]);
+    // Albers equal area conic projection for the US
+    var projection = d3.geoAlbers();
 
     var path = d3.geoPath()
         .projection(projection);
 
 
+    // create graticule
+    var graticule = d3.geoGraticule()
+        .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
+    // create graticule background
+    var gratBackground = map.append("path")
+    .datum(graticule.outline()) //bind graticule background
+    .attr("class", "gratBackground") //assign class for styling
+    .attr("d", path) //project graticule
 
-
+    // create graticule lines
+    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines
+        
 
     // use Promise.all to load all data asyncronously
     var promises = [
@@ -41,7 +51,7 @@ function setMap(){
     Promise.all(promises).then(callback);
 
     function callback(data){
-        // load attribute data
+        // load attribute data --- WILL REFORMAT ADD ALL DATA LATER
         var commuteMode = data[2];
 
         // temporarily load spatial data as topojson for conversion
@@ -50,14 +60,9 @@ function setMap(){
 
         // convert spatial data from topojson to geojson
         var worldCountries = topojson.feature(countryTemp, countryTemp.objects.countries_ne_50m),
-            usStates = topojson.feature(statesTemp, statesTemp.objects.us_states_natural_earth_generalized);
+            usStates = topojson.feature(statesTemp, statesTemp.objects.us_states_natural_earth_generalized).features;
 
-        //examine the results
-        console.log(worldCountries);
-        console.log(usStates);
-
-
-
+        
         //add world countries to map
         var countries = map.append("path")
             .datum(worldCountries)
@@ -65,7 +70,7 @@ function setMap(){
             .attr("d", path);
 
         //add us states to map
-        var states = map.selectAll(".regions")
+        var states = map.selectAll(".states")
             .data(usStates)
             .enter()
             .append("path")
@@ -73,7 +78,5 @@ function setMap(){
                 return "states " + d.properties.name;
             })
             .attr("d", path);
-
-
     };
 };
