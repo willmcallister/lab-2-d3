ec1968cef41858a330b0bb7bc7fc4f5c55733878
willmcallister <94069278+willmcallister@users.noreply.github.com>
2024-04-10 17:02:32 -0500

    joined attribute data to spatial data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// wrap everything in a self-executing anonymous function
+(function(){
 
 //execute script when window is loaded
 window.onload = setMap();
@@ -23,25 +25,9 @@ function setMap(){
         .projection(projection);
 
 
-    // create graticule
-    var graticule = d3.geoGraticule()
-        .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
-    // create graticule background
-    var gratBackground = map.append("path")
-    .datum(graticule.outline()) //bind graticule background
-    .attr("class", "gratBackground") //assign class for styling
-    .attr("d", path) //project graticule
-
-    // create graticule lines
-    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-        .data(graticule.lines()) //bind graticule lines to each element to be created
-        .enter() //create an element for each datum
-        .append("path") //append each element to the svg as a path element
-        .attr("class", "gratLines") //assign class for styling
-        .attr("d", path); //project graticule lines
+    //create graticule and place on map
+    createGraticule(map, path);
         
-
     // use Promise.all to load all data asyncronously
     var promises = [
         d3.json("data/countries_ne_50m.topojson"),
@@ -53,7 +39,6 @@ function setMap(){
     function callback(data){
         // load attribute data
         var csvData = data[2];
-        console.log(csvData);
 
         // temporarily load spatial data as topojson for conversion
         var countryTemp = data[0],
@@ -63,30 +48,36 @@ function setMap(){
         var worldCountries = topojson.feature(countryTemp, countryTemp.objects.countries_ne_50m),
             usStates = topojson.feature(statesTemp, statesTemp.objects.us_states_natural_earth_generalized).features;
 
-        console.log(usStates);
-
-        var attrArray = ["var1", "var2", "var3", "var4", "var5", "var6", "var7", "var8",
-            "var9", "var10", "var11", "var12", "var13", "var14", "var15", "var16", "var17"];
+        var attrArray = ["state","year","co2_emissions","transit_exp_local","highway_exp_local",
+            "transit_exp_state","highway_exp_state","commute_bike_pct","commute_carpool_pct",
+            "commute_drove_pct","commute_transit_pct","commute_taxi_pct","commute_walked_pct",
+            "commute_at_home_pct","transit_ridership","highway_gas_use","highway_vmt","vehicles,licensed_drivers"];
 
         
         //loop through csv to assign each set of csv attribute values to geojson state
         for (var i = 0; i < csvData.length; i++) {
             var csvState = csvData[i]; //the current state
-            var csvKey = csvState.State; //the CSV primary key -- might need to be lowercase
+            var csvKey = csvState.state; //the CSV primary key -- might need to be lowercase
     
             //loop through geojson regions to find correct state
             for (var a = 0; a < usStates.length; a++) {
-            var geojsonProps = usStates[a].properties; //the current state geojson properties
-            var geojsonKey = geojsonProps.name; //the geojson primary key
-    
-            //where primary keys match, transfer csv data to geojson properties object
-            if (geojsonKey == csvKey) {
-                //assign all attributes and values
-                attrArray.forEach(function (attr) {
-                var val = parseFloat(csvState[attr]); //get csv attribute value
-                geojsonProps[attr] = val; //assign attribute and value to geojson properties
-                });
-            }
+                var geojsonProps = usStates[a].properties; //the current state geojson properties
+                var geojsonKey = geojsonProps.name; //the geojson primary key
+
+                if(a === 0) {
+                    //console.log(csvState);
+                }
+                
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey) {                  
+                    //assign all attributes and values
+                    attrArray.forEach(function (attr) {
+                        var val = parseFloat(csvState[attr]); //get csv attribute value
+                        //console.log(csvState);
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                }
             }
         }
         console.log(usStates);
@@ -110,3 +101,27 @@ function setMap(){
             .attr("d", path);
     };
 };
+
+
+function createGraticule(map, path){
+    // create graticule
+    var graticule = d3.geoGraticule()
+        .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+    // create graticule background
+    var gratBackground = map.append("path")
+    .datum(graticule.outline()) //bind graticule background
+    .attr("class", "gratBackground") //assign class for styling
+    .attr("d", path) //project graticule
+
+    // create graticule lines
+    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines
+}
+
+
+})();
